92a71912a9dfe7cef4ccf82f3a4e135337e3dca1
Maruti <143268130+Maruti-Margale@users.noreply.github.com>
2025-08-26 13:45:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import numpy as np
 with open("svm_model_state26.pkl", "rb") as f:
     model = pickle.load(f)
 
-st.title("🍷 Wine Quality Prediction App By Dewansh01")
+st.title("🍷 Wine Quality Prediction App By Maruti")
 
 st.write("Enter the wine characteristics below to predict its quality.")
 
